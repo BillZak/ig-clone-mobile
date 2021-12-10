18b48f5aa388f853d706525ead3e51b1293c11f8
Bill Zack <bzak954@gmail.com>
2021-12-10 20:05:37 +0000

    remove navigation container

--- src/Component/navigator.js
+++ src/Component/navigator.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
 //import pages
 import HomePage from "../Pages/homePage";
 import SearchPage from "../Pages/searchPage";
@@ -15,87 +14,85 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarHideOnKeyboard: true,
-          contentStyle: { background: "white" },
-          headerShown: false,
-          tabBarActiveTintColor: "black",
-          tabBarStyle: [
-            {
-              display: "flex",
-            },
-            null,
-          ],
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        contentStyle: { background: "white" },
+        headerShown: false,
+        tabBarActiveTintColor: "black",
+        tabBarStyle: [
+          {
+            display: "flex",
+          },
+          null,
+        ],
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomePage}
+        options={{
+          tabBarIcon: (props) => (
+            <IconAntDesign
+              color={props.color}
+              style={styles.bottomicons}
+              name="home"
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
         }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomePage}
-          options={{
-            tabBarIcon: (props) => (
-              <IconAntDesign
-                color={props.color}
-                style={styles.bottomicons}
-                name="home"
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SearchPage}
-          options={{
-            tabBarIcon: (props) => (
-              <IconAntDesign
-                color={props.color}
-                style={styles.bottomicons}
-                name="search1"
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={Notification}
-          options={{
-            tabBarIcon: (props) => (
-              <IconAntDesign
-                color={props.color}
-                style={styles.bottomicons}
-                name="notification"
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-        <Tab.Screen
-          name="MyAccount"
-          component={MyAccount}
-          options={{
-            tabBarIcon: (props) => (
-              <IconFontAwesome
-                color={props.color}
-                style={styles.bottomicons}
-                name="user-circle-o"
-              />
-            ),
-            tabBarLabel: () => {
-              return null;
-            },
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchPage}
+        options={{
+          tabBarIcon: (props) => (
+            <IconAntDesign
+              color={props.color}
+              style={styles.bottomicons}
+              name="search1"
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: (props) => (
+            <IconAntDesign
+              color={props.color}
+              style={styles.bottomicons}
+              name="notification"
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="MyAccount"
+        component={MyAccount}
+        options={{
+          tabBarIcon: (props) => (
+            <IconFontAwesome
+              color={props.color}
+              style={styles.bottomicons}
+              name="user-circle-o"
+            />
+          ),
+          tabBarLabel: () => {
+            return null;
+          },
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
