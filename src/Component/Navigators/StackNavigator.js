d4d439d11a69b5b6219a5347de950aeb924b0268
Bill Zack <bzak954@gmail.com>
2021-12-14 14:45:18 +0000

    rename a page

--- src/Component/Navigators/StackNavigator.js
+++ src/Component/Navigators/StackNavigator.js
@@ -3,14 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import ReviewHomePost from "../../Pages/MainPages/Home/ReviewHomePost";
 import Main from "../../Pages/MainPages/Main";
-import Health from "../../Pages/MainPages/SearchPage/searchPages/Health";
-import Believe from "../../Pages/MainPages/SearchPage/searchPages/Believe";
-import HeartBreak from "../../Pages/MainPages/SearchPage/searchPages/HeartBreak";
-import Life from "../../Pages/MainPages/SearchPage/searchPages/Life";
-import Love from "../../Pages/MainPages/SearchPage/searchPages/Love";
-import Money from "../../Pages/MainPages/SearchPage/searchPages/Money";
-import Motivation from "../../Pages/MainPages/SearchPage/searchPages/Motivation";
-import Work from "../../Pages/MainPages/SearchPage/searchPages/Work";
+import ReviewSection from "../../Pages/MainPages/SearchPage/searchPages/ReviewSection";
 
 const Stack = createStackNavigator();
 
@@ -24,14 +17,7 @@ function MyStack() {
       >
         <Stack.Screen name="Main" component={Main} />
         <Stack.Screen name="ReviewHome" component={ReviewHomePost} />
-        <Stack.Screen name="Believe" component={Believe} />
-        <Stack.Screen name="Health" component={Health} />
-        <Stack.Screen name="HeartBreak" component={HeartBreak} />
-        <Stack.Screen name="Life" component={Life} />
-        <Stack.Screen name="Love" component={Love} />
-        <Stack.Screen name="Money" component={Money} />
-        <Stack.Screen name="Motivation" component={Motivation} />
-        <Stack.Screen name="Work" component={Work} />
+        <Stack.Screen name="ReviewSection" component={ReviewSection} />
       </Stack.Navigator>
     </NavigationContainer>
   );
