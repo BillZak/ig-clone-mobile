eaa67085ac5390f0774810a448b63566e951b728
Bill Zack <bzak954@gmail.com>
2022-01-13 15:55:53 +0000

    add message component

--- src/Pages/MainPages/Home/Message folder/Messages.js
+++ src/Pages/MainPages/Home/Message folder/Messages.js
@@ -7,13 +7,20 @@ import { Ionicons } from "@expo/vector-icons";
 const Messages = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Ionicons
-        onPress={() => navigation.goBack()}
-        name="arrow-back-outline"
-        size={24}
-        color="black"
-      />
-      <Text>Messages</Text>
+      <View style={styles.navbar}>
+        <Ionicons
+          onPress={() => navigation.goBack()}
+          name="arrow-back-outline"
+          size={30}
+          color="black"
+        />
+        <View>
+          <Text style={{ fontSize: 25, fontWeight: "bold", marginLeft: 20 }}>
+            Messages
+          </Text>
+        </View>
+      </View>
+      <View style={styles.messageboard}></View>
     </View>
   );
 };
@@ -22,8 +29,18 @@ const Messages = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: 30,
+  },
+  navbar: {
+    flex: 1,
+    flexDirection: "row",
     alignItems: "center",
+    borderBottomWidth: 0.5,
+    borderBottomColor: "silver",
+    paddingHorizontal: 20,
+  },
+  messageboard: {
+    flex: 11,
   },
 });
 
