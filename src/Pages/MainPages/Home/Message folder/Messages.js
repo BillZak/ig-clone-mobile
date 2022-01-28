c6ecf480edd04e41fec757889e7eccb71ed68bc2
Bill Zack <bzak954@gmail.com>
2022-01-28 15:53:19 +0000

    remove flatlist

--- src/Pages/MainPages/Home/Message folder/Messages.js
+++ src/Pages/MainPages/Home/Message folder/Messages.js
@@ -1,20 +1,7 @@
 //import liraries
-import React, { Component } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
-const DATA = [
-  { id: "1", name: "bill zak", time: "12:00", img: require(""), message: "where u dey" },
-  { id: "2",name: "bill zak", time: "12:00", img: require(""), message: "where u dey" },
-  { id: "3",name: "bill zak", time: "12:00", img: require(""), message: "where u dey" },
-  { id: "4",name: "bill zak", time: "12:00", img: require(""), message: "where u dey" },
-];
 
 // create a component
 const Messages = ({ navigation }) => {
@@ -33,23 +20,6 @@ const Messages = ({ navigation }) => {
           </Text>
         </View>
       </View>
-
-      <FlatList
-        style={styles.flatlist}
-        data={DATA}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => {
-          return (
-            <TouchableOpacity
-              key={item.id}
-              onPress={}
-              style={styles.item}
-            >
-              <View style={styles.pinkbox}></View>
-            </TouchableOpacity>
-          );
-        }}
-      />
     </View>
   );
 };
@@ -58,7 +28,7 @@ const Messages = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 30,
+    paddingTop: 20,
   },
   navbar: {
     flex: 1,
