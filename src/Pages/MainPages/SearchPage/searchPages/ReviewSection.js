412e640160672bd1c5747c9d56fd3e684060d23e
Bill Zack <bzak954@gmail.com>
2022-01-28 15:53:34 +0000

    add styles to page

--- src/Pages/MainPages/SearchPage/searchPages/ReviewSection.js
+++ src/Pages/MainPages/SearchPage/searchPages/ReviewSection.js
@@ -36,7 +36,7 @@ const ReviewSection = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 30,
+    paddingTop: 20,
   },
   face1: {
     flexDirection: "row",
